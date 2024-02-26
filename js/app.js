cfa77d1e1b923a2e9a6286a34e0d3af29f0ba637
GvidoGvido <149728353+GvidoGvido@users.noreply.github.com>
2024-02-26 08:31:14 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+yaakovGreeter.sayHello();
+johnGreeter.sayHi();
+
+// // immmediately invoked function expression or IIFE
+// (function (name) {
+// 	console.log("Hello " + name);
+// })("Coursera");
